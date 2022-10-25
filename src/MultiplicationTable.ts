e52695659731ea2a7927b12e281212a575546cd2
Xiaotian WU <XWu32@slb.com>
2022-10-25 17:27:13 +0800

    feat: finish function

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,38 @@
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
+  public render(start: number, end: number): string {
+    if (this.isStartLessThanEnd(start, end)) {
+      return ''
+    }
+
+    if (!this.isValidRange(start, end)) {
+      return ''
+    }
+
+    return this.getMultiplicationTable(start, end)
+  }
+
+  private isStartLessThanEnd(start: number, end: number): boolean {
+    return start > end
+  }
+
+  private isValidRange(start: number, end: number): boolean {
+    return start >= 1 && end < 10
+  }
+
+  public getMultiplicationTable(start: number, end: number) {
+    let multiplicationTable = ''
+    for (let coulmn = start; coulmn <= end; coulmn++) {
+      for (let row = start; row <= coulmn; row++) {
+        const tmp = row + '*' + coulmn + '=' + row * coulmn
+        multiplicationTable = multiplicationTable + tmp
+        if (row !== coulmn) {
+          multiplicationTable = multiplicationTable + '  '
+        }
+        if (row === coulmn && row !== end) {
+          multiplicationTable = multiplicationTable + '\n'
+        }
+      }
+    }
+    return multiplicationTable
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,4 +1,4 @@
-import {MultiplicationTable} from '../src/MultiplicationTable'
+import { MultiplicationTable } from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
@@ -12,5 +12,18 @@ describe('Multiplication Table', () => {
 
     //then
     expect(rendered).toBe('1*1=1')
+  })
+
+  it('should render multiplication table of (2, 4)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 4
+
+    //when
+    const rendered = table.render(start, end)
+    console.log('rendered: ', rendered)
+    //then
+    expect(rendered).toBe('2*2=4\n2*3=6  3*3=9\n2*4=8  3*4=12  4*4=16')
   })
 })
